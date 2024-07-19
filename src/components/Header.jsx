d7e8e3aad7d9c53bfb2bd1630c5808a6543154ca
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2024-07-19 19:08:55 +0530

    changes by Asim

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -140,7 +140,7 @@ const Header = () => {
               </li>
               <li className="nav-item dropdown">
               
-  <button  data-bs-toggle="dropdown" aria-expanded="false">
+  <button className='btn'  data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown button
   </button>
   <ul class="dropdown-menu">
@@ -232,17 +232,6 @@ const Header = () => {
         </div>
       </nav>
 
-
-      <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-  </ul>
-</div>
       
     </>
   );
